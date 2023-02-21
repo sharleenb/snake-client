db2db51e19a85832c14c5244b11dc06272815cf4
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-21 02:39:14 +0000

    import connect function

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
 const net = require("net");
+const connect = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
