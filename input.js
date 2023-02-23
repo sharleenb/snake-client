ea8cc0ab99ef4e4919916397e7c257368718e9b2
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-23 02:02:51 +0000

    add keyboard controls to move snake and function accepts connection parameter

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 // setup interface to handle user input from stdin
+let connection;
 
-const setUpInput = function () {
+const setUpInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,6 +11,18 @@ const setUpInput = function () {
   const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
+    }
+    if (key === 's') {
+      conn.write("Move: down");
+    }
+    if (key === 'd') {
+      conn.write("Move: right");
     }
   };
 
